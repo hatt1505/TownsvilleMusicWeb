de04703730f211c3ce8c1d2cd0794a4196087f13
hatt1505 <thuha.tran@my.jcu.edu.au>
2018-01-20 23:46:01 +0800

    add event pages

--- site/js/listgrid.js
+++ site/js/listgrid.js
@@ -21,12 +21,13 @@ function gridView() {
 }
 
 /* Optional: Add active class to the current button (highlight it) */
+/* Optional: Add active class to the current button (highlight it) */
 var container = document.getElementById("btnContainer");
 var btns = container.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+	btns[i].addEventListener("click", function(){
+    	var current = document.getElementsByClassName("active");
+    	current[0].className = current[0].className.replace(" active", "");
+    	this.className += " active";
+	});
 }
